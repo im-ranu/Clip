30f2179f32dec745ab7576d742ccd07f4c3b59cf
im-ranu <ranuluhar@gmail.com>
2019-03-25 19:10:06 +0530

    Changes

---
+++ app/build/generated/source/buildConfig/release/com/guysapp/clips/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.guysapp.clips;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.guysapp.clips";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
